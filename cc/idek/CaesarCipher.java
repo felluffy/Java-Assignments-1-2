8c7a7ee1c74d48f71c04bb1dd19e13cedbcc1ae4
felluffy <somerandomghai@gmail.com>
2017-10-08 21:18:25 +0600

    add encryptTwoKeys method that encodes a message by two keys depending on the position

--- cc/idek/CaesarCipher.java
+++ cc/idek/CaesarCipher.java
@@ -15,7 +15,7 @@ public class CaesarCipher {
 		StringBuilder ret = new StringBuilder(input);
 		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String encryptedForm = alphabet.substring(key, 26) + alphabet.substring(0, key);
-		System.out.println(encryptedForm);
+		//System.out.println(encryptedForm);
 		for(int i = 0; i != ret.length(); i++)
 		{
 			if(Character.isAlphabetic(ret.charAt(i)))
@@ -31,10 +31,35 @@ public class CaesarCipher {
 	
 	void testCaesar()
 	{
+		/*
 		int key = 23;
 		FileResource fr = new FileResource();
 		String message = fr.asString();
 		String encrypted = encrypt(message, key);
 		System.out.println("key is: " + key + "\n" + encrypted);
+		*/
+		System.out.println(encrypt("First Legion", 17));
+		System.out.println(encryptTwoKeys("First Legion", 23, 17));
+	}
+	
+	String encryptTwoKeys(String input, int key1, int key2)
+	{
+		StringBuilder ret = new StringBuilder(input);
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String [] encryptedForm = new String[2];
+		encryptedForm[0] = alphabet.substring(key1, 26) + alphabet.substring(0, key1);
+		encryptedForm[1] = alphabet.substring(key2, 26) + alphabet.substring(0, key2);
+		int currIndex = 1;
+		for(int i = 0; i != ret.length(); i++)
+		{
+			if(Character.isAlphabetic(ret.charAt(i)))
+			{
+				if(Character.isUpperCase(ret.charAt(i)))
+					ret.setCharAt(i, encryptedForm[i%2].charAt(ret.charAt(i) - 'A'));
+				else
+					ret.setCharAt(i, Character.toLowerCase(encryptedForm[i%2].charAt(Character.toUpperCase(ret.charAt(i)) - 'A')));
+			}
+		}
+		return ret.toString();
 	}
 }
