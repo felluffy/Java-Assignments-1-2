b324a28d68ea4f132e63675e4035dfed97b7cb31
felluffy <somerandomghai@gmail.com>
2017-10-30 01:35:53 +0600

    extend WordLength method to count the number of words of each possible length upto a certain value and a test method to test it and indexOfMax that returns the max value out of the possible length array

--- cc/idek/DecodingCaesarCipher.java
+++ cc/idek/DecodingCaesarCipher.java
@@ -1,25 +1,72 @@
 package com.cc.idek;
 import edu.duke.*;
-public class DecodingCaesarCipher {
+public class DecodingCaesarCipher 
+{
 
-	public int[] WordLength(FileResource resource, int[] counts)
+	public void WordLength(FileResource resource, int[] counts)
 	{
 		for(String words: resource.words())
 
 		{
-			String alphabet = "abcdefghijklmnopqrstuvwxyz";
 			if(words.length() == 1 && Character.isLetter(words.charAt(0)))	//if the character isn't a letter of size 1
 				continue;
 			else
 			{
-				words.in
+				
+				String build = new String();
+				for(int i = 0; i != words.length(); i++)
+				{
+					if((i == 0 || i == words.length() - 1) && !Character.isAlphabetic(words.charAt(i)))
+							continue;
+					else 
+						build += words.charAt(i);		
+				}
+				
+				//System.out.println(build);
+				if(build.length() < counts.length)
+					++counts[build.length()];
+				else
+					++counts[counts.length - 1];
+				//if(!Character.isAlphabetic(words.charAt(0)) || !Character.isAlphabetic(words.charAt(0)))
 			}
 		}
-		return new int [21];
+		//return new int [21];
 	}
-	public static void main(String[] args) {
+	
+	public int indexOfMax(int[] counts)
+	{
+		int max = Integer.MIN_VALUE;
+		for(int i = 0 ; i != counts.length; i++)
+		{
+			if(counts[i] > max)
+				max = counts[i];
+		}
+		return max;
+	}
+	
+	public void testCountWordLengths()
+	{
+		FileResource fr;
+		fr = new FileResource();
+		int[] counts = new int[31];
+		WordLength(fr, counts);
+		int i = 0;
+		for(int value : counts)
+		{	
+			if(value != 0)
+				System.out.println(value + " words of length " + i);
+			i++;
+		}
+		int max = indexOfMax(counts);
+		System.out.println("maximum average word size is: " + max);
+		
+	}
+	
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		DecodingCaesarCipher blyat = new DecodingCaesarCipher();
+		blyat.testCountWordLengths();
 	}
 
 }
