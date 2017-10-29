8fa9a26748f50b9ed0f2b33d14b41127c329a416
felluffy <somerandomghai@gmail.com>
2017-10-30 00:56:19 +0600

    added partial WordLength method

---
+++ cc/idek/DecodingCaesarCipher.java
@@ -0,0 +1,25 @@
+package com.cc.idek;
+import edu.duke.*;
+public class DecodingCaesarCipher {
+
+	public int[] WordLength(FileResource resource, int[] counts)
+	{
+		for(String words: resource.words())
+
+		{
+			String alphabet = "abcdefghijklmnopqrstuvwxyz";
+			if(words.length() == 1 && Character.isLetter(words.charAt(0)))	//if the character isn't a letter of size 1
+				continue;
+			else
+			{
+				words.in
+			}
+		}
+		return new int [21];
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
