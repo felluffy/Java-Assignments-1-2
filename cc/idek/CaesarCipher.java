ffe3f12d3516b003522b3512923b832f2391f91b
felluffy <somerandomghai@gmail.com>
2018-01-22 20:41:12 +0600

    create CaesarCipher class in a more object oriented way

---
+++ cc/idek/CaesarCipher.java
@@ -0,0 +1,17 @@
+package com.cc.idek;
+
+public class CaesarCipher 
+{
+	CaesarCipher(int key)
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+
+	}
+	private String alphabet;
+	private String shiftedAlphabet;
+	
+}
