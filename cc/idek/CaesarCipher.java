70659226cbe3c44996552ec94d400257087ad86b
felluffy <somerandomghai@gmail.com>
2018-01-22 22:05:30 +0600

    complete object oriented caesar cipher with 1 key and create with 2 key

--- cc/idek/CaesarCipher.java
+++ cc/idek/CaesarCipher.java
@@ -4,13 +4,41 @@ public class CaesarCipher
 {
 	CaesarCipher(int key)
 	{
-		
+		mainKey = key;
+		alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		shiftedAlphabet = alphabet.substring(key, 26) + alphabet.substring(0, key);
+	}
+	
+	String encrypt(String input)
+	{
+		StringBuilder ret = new StringBuilder(input);
+		for(int i = 0; i != ret.length(); i++)
+		{
+			if(Character.isAlphabetic(ret.charAt(i)))
+			{
+				if(Character.isUpperCase(ret.charAt(i)))
+					ret.setCharAt(i, shiftedAlphabet.charAt(ret.charAt(i) - 'A'));
+				else
+					ret.setCharAt(i, Character.toLowerCase(shiftedAlphabet.charAt(Character.toUpperCase(ret.charAt(i)) - 'A')));
+			}
+		}
+		return ret.toString();
+	}
+	
+	String decrypt(String input)
+	{
+		CaesarCipher cc = new CaesarCipher(26 - mainKey);
+		return cc.encrypt(input);
 	}
 	
 	public static void main(String[] args) 
 	{
-
+		CaesarCipher cc = new CaesarCipher(23);
+		System.out.println(cc.encrypt("First Legion"));
+		System.out.println(cc.decrypt(cc.encrypt("First Legion")));
 	}
+	
+	int mainKey;
 	private String alphabet;
 	private String shiftedAlphabet;
 	
---
+++ cc/idek/CaesarCipherTwo.java
@@ -0,0 +1,14 @@
+package com.cc.idek;
+
+public class CaesarCipherTwo 
+{
+	public CaesarCipherTwo(int key1, int key2)
+	{	
+		
+	}
+	private String alphabet;
+    private String shiftedAlphabet1;
+    private String shiftedAlphabet2;
+    private int mainKey1;
+    private int mainKey2;
+}
